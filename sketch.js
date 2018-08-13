68cd98face8a791a1a9a47775c4c0569cc7425e1
VictorPersson <vp-96@hotmail.com>
2018-08-13 18:10:32 +0200

    Implemented a second slider for better clap control

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
 var bird;
 var pipes = [];
 var mic;
-var slider;
+var sliderBottom;
+var sliderTop;
+var clapping = false;
 
 // Creating the sketch canvas (size of the play area window)
 function setup() {
@@ -12,8 +14,9 @@ function setup() {
   mic.start();
   bird = new Bird();
   pipes.push(new Pipe());
-  // Generates a slider with an increse of 0.01
-  slider = createSlider(0, 1, 0.2, 0.01);
+  // Generates two slider with an increse of 0.01
+  sliderBottom = createSlider(0, 1, 0.1, 0.01);
+  sliderTop = createSlider(0, 1, 0.4, 0.01);
 }
 
 function draw() {
@@ -43,21 +46,33 @@ function draw() {
     pipes.push(new Pipe());
   }
 
-  var threshold = slider.value();
+  var thresholdTop = sliderTop.value();
+  var thresholdBottom = sliderBottom.value();
 
-  if (vol > threshold) {
+  if (vol > thresholdTop && !clapping) {
     bird.up();
+    clapping = true;
   } 
+
+  // If the clapping sound level ever gets below the bottom threshold, set it to false
+  if (vol < thresholdBottom) {
+    clapping = false;
+  }
   // Drawing a rectangle which will fill up with a green color wehn volume is picked up
   fill(0, 190 , 0);
-  var y = map(vol, 0, 2, height, 0);
-  rect(width - 50, y, 50, height - y);
+  var topY = map(vol, 0, 2, height, 0);
+  rect(width - 50, topY, 50, height - topY);
 
   // Drawing the threshold the slider controls
-  var ty = map(threshold, 0, 1, height, 0);
+  var topY = map(thresholdTop, 0, 1, height, 0);
   stroke(255, 0, 0);
   strokeWeight(5);
-  line(width -50, ty, width, ty);
+  line(width -50, topY, width, topY);
+
+  var bottomY = map(thresholdBottom, 0, 1, height, 0);
+  stroke(0, 0, 255);
+  strokeWeight(5);
+  line(width -50, bottomY, width, bottomY);
 
   
 }
